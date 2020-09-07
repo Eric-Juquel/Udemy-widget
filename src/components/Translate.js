ba8df4afcf6977dd76cbca1d6f1b4032ded829f1
eric J <e.juquel@woody-technologies.com>
2020-09-07 21:11:56 +0200

    traducteur

---
+++ src/components/Translate.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Dropdown from "./Dropdown";
+import Convert from './Convert'
+
+
+
+const options = [
+  {
+    label: "Afrikaans",
+    value: "af",
+  },
+  {
+    label: "Arabic",
+    value: "ar",
+  },
+  {
+    label: "Hindi",
+    value: "hi",
+  },
+  {
+    label: "malgache",
+    value: "mg",
+  },
+];
+
+const Translate = () => {
+  const [langage, setLangage] = useState(options[0]);
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Text</label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
+      <Dropdown
+        label="Select a Language"
+        options={options}
+        selected={langage}
+        onSelectedChange={setLangage}
+      />
+      <hr />
+      <h3 className="ui header">Output</h3>
+      <Convert langage={langage} text={text}/>
+    </div>
+  );
+};
+
+export default Translate;
